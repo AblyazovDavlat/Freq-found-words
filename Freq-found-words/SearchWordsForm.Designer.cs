831d205907cd81669f829d4202c67ba65463e2ce
AblyazovDavlat <ablyazovdavlat@gmail.com>
2019-04-10 21:44:29 +0300

    Implemented project

---
+++ Freq-found-words/SearchWordsForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Freq_found_words
+{
+    partial class SearchWords
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.filePathLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 33);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(219, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // filePathLabel
+            // 
+            this.filePathLabel.AutoSize = true;
+            this.filePathLabel.Location = new System.Drawing.Point(13, 14);
+            this.filePathLabel.Name = "filePathLabel";
+            this.filePathLabel.Size = new System.Drawing.Size(47, 13);
+            this.filePathLabel.TabIndex = 1;
+            this.filePathLabel.Text = "File path";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(237, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 44);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.outputTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 244);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(6, 19);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(288, 219);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.Text = "";
+            // 
+            // SearchWords
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 326);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.filePathLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "SearchWords";
+            this.Text = "SearchWords";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label filePathLabel;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+    }
+}
+
---
+++ Freq-found-words/SearchWordsForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Freq_found_words
+{
+    public partial class SearchWords : Form
+    {
+        public SearchWords()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string filePath;
+            string[] readFile;
+            Dictionary<string, int> resultMap = new Dictionary<string, int>();
+
+            if (inputTextBox.Text.Length != 0)
+            {
+                filePath = inputTextBox.Text;
+            }
+            else
+            {
+                outputTextBox.Text = "Please. enter path ro input file";
+                return;
+            }
+
+            if (File.Exists(filePath))
+            {
+                readFile = File.ReadAllLines(filePath);
+            }
+            else
+            {
+                outputTextBox.Text = "File is not exist";
+                return;
+            }
+
+            resultMap = CountSameWords(readFile);
+
+            outputTextBox.Text = "";
+            foreach (KeyValuePair<string, int> wordQuantityPair in resultMap.OrderByDescending(key => key.Value))
+            {
+                outputTextBox.AppendText(wordQuantityPair.Key + " " + wordQuantityPair.Value + "\n");
+            }
+        }
+
+        private Dictionary<string, int> CountSameWords (string[] readFile)
+        {
+            Dictionary<string, int> wordsMap = new Dictionary<string, int>();
+
+            foreach (string str in readFile)
+            {
+                string _newString = Regex.Replace(str, "[-.?!)(,:]", "").ToLower();
+                string[] _words = _newString.Split(' ');
+                foreach (string word in _words)
+                {
+                    if (!wordsMap.ContainsKey(word))
+                    {
+                        wordsMap.Add(word, 1);
+                    }
+                    else
+                    {
+                        wordsMap[word]++;
+                    }
+                }
+            }
+
+            return wordsMap;
+        }
+    }
+}
